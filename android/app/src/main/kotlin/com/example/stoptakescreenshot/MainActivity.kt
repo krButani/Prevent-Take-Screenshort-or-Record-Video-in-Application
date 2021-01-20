4a8d4fdf3b2ccc73ac7b27ce4888daf13cae602e
krButani <butanikartik1108@gmail.com>
2021-01-20 16:37:54 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/stoptakescreenshot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stoptakescreenshot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+
+// iso link https://medium.com/nerd-for-tech/prevent-screenshot-and-video-recording-in-flutter-93839325d66c
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<void> secureScreen() async {
+    await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+  }
+
+  @override
+  void initState() {
+    secureScreen();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Stop Taking Screen Short"),
+        ),
+        body: Center(
+          child: Text("You Can't Take Screen Short or Record Video"),
+        ),
+      ),
+    );
+  }
+}
